56832373a1b1fda1de3a33ae1c17af5239979d8c
milanzagorac <zagoracm@gmail.com>
2021-06-08 20:03:01 -0400

    made a small change - images don't work

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
-function Pokecard(props) {
+function Pokecard({ name, id, type, exp }) {
 
     const imgURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon'
 
     return (
         <div>
-            <h4>{ props.name }</h4>
-            <img src={`imgURL/${props.id}`} href="pokemon image" />
-            <h4>Type: { props.type} </h4>
-            <h4>Exp: <span>{ props.exp }</span></h4>
+            <h4>{ name }</h4>
+            <img src={`imgURL/${ id }.png`} href="pokemon image" />
+            <h4>Type: { type } </h4>
+            <h4>Exp: <span>{ exp }</span></h4>
         </div>
     )
 }
